1aa0f68656de712c4ae79166846257e479136b28
CorentinPolard <corentinpolard@gmail.com>
2025-10-21 14:14:20 +0200

    Optimisation du script pierre-feuille-ciseaux

--- js/script.js
+++ js/script.js
@@ -1,42 +1,25 @@
 function pierreFeuilleCiseaux() {
     const affichage = document.querySelector(".affichage");
-    const choixAvatar = document.querySelector(".choixAvatar"); 
+    const choixAvatar = document.querySelector(".choixAvatar");
     const choix = [["Pierre", "./images/pierre.svg"], ["Feuille", "./images/feuille.svg"], ["Ciseaux", "./images/sciseaux.svg"]]; // Initialise les choix de l'ordinateur
     const index = Math.floor(Math.random() * 3); // Génère un nombre aléatoire entre 0 et 2  
-    
+
     const ordi = choix[index][0]; // L'action de l'ordi
-    
+
     let resultat = "";
-    
+
     if (this.id === ordi) {
         resultat = "égalité !"
     }
-    else if (this.id === "Pierre") {
-        if (ordi === "Feuille") {
-            resultat = "tu as perdu !"
-        }
-        else {
-            resultat = "tu as gagné !"
-        }
-    }
-    else if (this.id === "Feuille") {
-        if (ordi === "Ciseaux") {
-            resultat = "tu as perdu !"
-        }
-        else {
-            resultat = "tu as gagné !"
-        }
-    }
-    else {
-        if (ordi === "Pierre") {
-            resultat = "tu as perdu !"
-        }
-        else {
-            resultat = "tu as gagné !"
-        }
+    else if ((this.id === "Pierre" && ordi === "Feuille")
+        || (this.id === "Ciseaux" && ordi === "Pierre")
+        || (this.id === "Feuille" && ordi === "Ciseaux")) {
+        resultat = "tu as perdu !"
+    } else {
+        resultat = "tu as gagné !"
     }
     affichage.innerHTML = `J'ai choisi ${ordi}, ${resultat}`;
-    choixAvatar.src= choix[index][1];
+    choixAvatar.src = choix[index][1];
     choixAvatar.classList.remove("hide");
     return;
 }
